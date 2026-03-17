ac6ec60ef1bf5a829393d4ca0235c705f80f011d
Margaux Reydon <margauxer10@gmail.com>
2026-03-17 15:44:04 -0700

    Day 5

--- Assets/Scripts/Corgi.cs
+++ Assets/Scripts/Corgi.cs
@@ -1,39 +1,113 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Corgi : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
+    public Sprite DrunkSprite;
+    public Sprite SoberSprite;
+    
+    private SpriteRenderer corgiSpriteRenderer;
+    private bool isDrunk = false;
+    private Coroutine soberupCoroutine;
 
     public void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        corgiSpriteRenderer = GetComponent<SpriteRenderer>();
     }
+    
     public void Move(Vector2 direction)
     {
+        direction = ApplyDrunkenness(direction);
+        
         FaceCorrectDirection(direction);
         
         Vector2 movementAmount = GameParameters.CorgiMoveSpeed * direction * Time.deltaTime;
         
-        spriteRenderer.transform.Translate(movementAmount.x, movementAmount.y, 0);
+        corgiSpriteRenderer.transform.Translate(movementAmount.x, movementAmount.y, 0);
 
-		spriteRenderer.transform.position = SpriteTools.ConstrainToScreen(spriteRenderer);
+		corgiSpriteRenderer.transform.position = SpriteTools.ConstrainToScreen(corgiSpriteRenderer);
 
+    }
+
+    private Vector2 ApplyDrunkenness(Vector2 direction)
+    {
+        if (isDrunk)
+        {
+            direction.x = direction.x * -1;
+            direction.y = direction.y * -1;
+        }
+        
+        return direction;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Beer")
+        {
+            GetDrunk();
+            Destroy(other.gameObject);
+        }
+        else if (other.tag == "Bone")
+        {
+            print("do bone things");
+        }
+        else if (other.tag == "Pill")
+        {
+            print("do pill things");
+        }
+    }
+
+    private void GetDrunk()
+    {
+        isDrunk = true;
+        ChangeToDrunkSprite();
+        StartSoberingUp();
+    }
+
+    private void StartSoberingUp()
+    {
+        if (soberupCoroutine != null)
+            StopCoroutine(soberupCoroutine);
+        soberupCoroutine = StartCoroutine(CountdownUntilSober());
+    }
+
+    IEnumerator CountdownUntilSober()
+    {
+        yield return new WaitForSeconds(GameParameters.CorgiDrunkSeconds);
+        SoberUp();
+    }
+
+    private void SoberUp()
+    {
+        ChangeToSoberSprite();
+        isDrunk = false;
+    }
+
+    private void ChangeToSoberSprite()
+    {
+        corgiSpriteRenderer.sprite = SoberSprite;
+    }
+
+    private void ChangeToDrunkSprite()
+    {
+        corgiSpriteRenderer.sprite = DrunkSprite;
     }
 
     private void FaceCorrectDirection(Vector2 direction)
     {
         if (direction.x > 0) // right
         {
-            spriteRenderer.flipX = false;
+            corgiSpriteRenderer.flipX = false;
         }
         else if (direction.x < 0) //left
         {
-            spriteRenderer.flipX = true;
+            corgiSpriteRenderer.flipX = true;
         }
     }
 
 	public Vector3 GetPosition() 
 	{
-		return spriteRenderer.transform.position;
+		return corgiSpriteRenderer.transform.position;
 	}
 }
