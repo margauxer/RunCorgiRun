cf1da01491d3e2ac0e24c2f17793b23e1980e977
Margaux Reydon <margauxer10@gmail.com>
2026-04-02 15:39:27 -0700

    Day 8

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,23 +3,40 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public UI Ui;
+    public GameTimer GameTimer;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Ui.HideGameOverScreen();
+        Ui.ShowStartScreen();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ui.ShowTime();
     }
 
     public void OnStartButtonClicked()
     {
         Ui.HideStartScreen();
-        // start game
+        StartGame();
     }
-    
+
+    public void OnPlayAgainButtonClicked()
+    {
+        Ui.HideGameOverScreen();
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        GameTimer.StartTimer(10, OnTimerFinished);
+    }
+
+    public void OnTimerFinished()
+    {
+        Ui.ShowGameOverScreen();
+    }
 }
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class UI : MonoBehaviour
 {
     public TMP_Text scoreText;
+    public TMP_Text TimeText;
     public CanvasGroup StartScreenCanvasGroup;
+    public CanvasGroup GameOverScreenCanvasGroup;
+    public GameTimer GameTimer;
     
     public void SetScoreText(int score)
     {
@@ -14,5 +17,25 @@ public class UI : MonoBehaviour
     public void HideStartScreen()
     {
         CanvasGroupDisplayer.Hide(StartScreenCanvasGroup);
+    }
+
+    public void HideGameOverScreen()
+    {
+        CanvasGroupDisplayer.Hide(GameOverScreenCanvasGroup);
+    }
+    
+    public void ShowGameOverScreen()
+    {
+        CanvasGroupDisplayer.Show(GameOverScreenCanvasGroup);
+    }
+    
+    public void ShowStartScreen()
+    {
+        CanvasGroupDisplayer.Show(StartScreenCanvasGroup);
+    }
+
+    public void ShowTime()
+    {
+        TimeText.text = GameTimer.GetTimeAsString();
     }
 }
